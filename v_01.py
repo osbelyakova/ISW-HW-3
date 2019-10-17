0030656d59e96f04dadb29fd288645b9474b9e42
osbelyakova <55315386+osbelyakova@users.noreply.github.com>
2019-10-17 22:14:33 +0300

    Update v_01.py

--- v_01.py
+++ v_01.py
@@ -8,16 +8,19 @@ def check_folder(new_path, our_folder):
     """Looking for a 'm4a' file and build the catalog tree"""
     for filename in os.listdir(new_path):
         if filename[filename.rfind(".") + 1:] == 'm4a':
-            x, sr = librosa.load(new_path + '\\' + filename)
+            full_path = os.path.join(new_path, filename)
+            x, sr = librosa.load(full_path)
             mfcc = librosa.feature.mfcc(x, sr)
-            np.save(our_folder + '\\' + filename[0:filename.rfind("."):], mfcc)
+            full_path = os.path.join(our_folder, filename[0:filename.rfind("."):])
+            np.save(full_path, mfcc)
         else:
-            os.makedirs(our_folder + '\\' + filename)
-            check_folder(new_path + '\\' + filename, our_folder + '\\' + filename)
+            full_path_1 = os.path.join(our_folder, filename)
+            os.makedirs(full_path)
+            full_path_2 = os.path.join(new_path, filename)
+            check_folder(full_path_2, full_path_1)
             continue
 start_time = time.time()
-#os.chdir('C:\\Users\\osbel\\Desktop')
-answer_path = os.getcwd() + '\\with_MFCC'
+answer_path = os.path.join(os.getcwd(), 'with_MFCC')
 os.mkdir(answer_path)
 check_folder(os.path.abspath(NULL_PATH), answer_path)
 print("Time: {}".format(time.time() - start_time))
