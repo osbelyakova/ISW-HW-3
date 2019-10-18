ace8b122c3d15b6f918a6eaac21b36fa6d6b851b
osbelyakova <55315386+osbelyakova@users.noreply.github.com>
2019-10-18 08:45:16 +0300

    Update v_02.py

--- v_02.py
+++ v_02.py
@@ -10,21 +10,23 @@ def check_folder(new_path, our_folder):
     threads = []
     for filename in os.listdir(new_path):
         if filename[filename.rfind(".") + 1:] == 'm4a':
-            y, sr = librosa.load(new_path + '\\' + filename)
-            mfcc = librosa.feature.mfcc(y, sr)
-            np.save(our_folder + '\\' + filename[0:filename.rfind("."):], mfcc)
+            full_path = os.path.join(new_path, filename)
+            x, sr = librosa.load(full_path)
+            mfcc = librosa.feature.mfcc(x, sr)
+            full_path = os.path.join(our_folder, filename[0:filename.rfind("."):])
+            np.save(full_path, mfcc)
         else:
-            os.makedirs(our_folder + '\\' + filename)
-            t1 = Thread(target=check_folder, args=(new_path + '\\' + filename, our_folder + '\\' + filename,))
+            full_path_1 = os.path.join(our_folder, filename)
+            os.makedirs(full_path_1)
+            full_path_2 = os.path.join(new_path, filename)
+            t1 = Thread(target=check_folder, args=(full_path_2, full_path_1,))
+            t1.start()
             threads.append(t1)
             continue
     for t1 in threads:
-        t1.start()
-    for t1 in threads:
         t1.join()
 start_time = time.time()
-#os.chdir('C:\\Users\\osbel\\Desktop')
-answer_path = os.getcwd() + '\\with_MFCC'
+answer_path = os.path.join(os.getcwd(), 'with_MFCC')
 os.mkdir(answer_path)
 check_folder(os.path.abspath(NULL_PATH), answer_path)
 print("Time: {}".format(time.time() - start_time))
