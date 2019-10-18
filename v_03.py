5562278d239150f0377b2549d2e679b3acea4a46
osbelyakova <55315386+osbelyakova@users.noreply.github.com>
2019-10-18 09:28:54 +0300

    Update v_03.py

--- v_03.py
+++ v_03.py
@@ -2,40 +2,33 @@ import numpy as np
 import librosa
 import os
 import time
-from multiprocessing import Process, current_process
+from multiprocessing import Process
+"""Take MFCC and put it in the catalog"""
 NULL_PATH = "id00012"
-
 def check_folder(new_path, our_folder):
-    procs = []
-    print(current_process().name)
+    """Looking for a 'm4a' file and build the catalog tree"""
+    threads = []
     for filename in os.listdir(new_path):
-        os.chdir(new_path)
-        if os.path.isdir(os.path.abspath(filename)):
-            new_folder = our_folder + '\\'
-            new_folder = new_folder + filename
-            os.makedirs(new_folder)
-            full_path = os.path.abspath(filename)
-            proc = Process(target=check_folder,name = filename, args=(full_path, new_folder,))
-            procs.append(proc)
-            proc.start()
-            continue
-        ext = filename[filename.rfind(".") + 1:]
-        if ext == 'm4a':
-            os.rename(filename, filename[0:filename.rfind(".") + 1:] + 'ogg')
-            filename = filename[0:filename.rfind(".") + 1:] + 'ogg'
-            ext = 'ogg'
-        if ext == 'ogg':
-            x , sr = librosa.load(os.path.abspath(filename))
+        if filename[filename.rfind(".") + 1:] == 'm4a':
+            full_path = os.path.join(new_path, filename)
+            x, sr = librosa.load(full_path)
             mfcc = librosa.feature.mfcc(x, sr)
-            new_folder = our_folder + '\\'
-            new_folder = new_folder + filename[0:filename.rfind("."):]
-            np.save(new_folder, mfcc)
-    print(procs)
-    for proc in procs:
-        proc.join()
-start_time = time.time()
-os.chdir('C:\\Users\\osbel\\Desktop')
-answer = os.getcwd() + '\\test'
-os.mkdir(answer)
-check_folder(os.path.abspath(NULL_PATH), answer)
-print("Time: {}".format(time.time() - start_time))
+            full_path = os.path.join(our_folder, filename[0:filename.rfind("."):])
+            np.save(full_path, mfcc)
+        else:
+            full_path_1 = os.path.join(our_folder, filename)
+            os.makedirs(full_path_1)
+            full_path_2 = os.path.join(new_path, filename)
+            t1 = Process(target=check_folder, args=(full_path_2, full_path_1,))
+            threads.append(t1)
+            continue
+    for i in range(threads.len()):
+        theads[i].start()
+    for i in range(threads.len()):
+        theads[i].join()
+if __name__ == '__main__':
+    start_time = time.time()
+    answer_path = os.path.join(os.getcwd(), 'with_MFCC')
+    os.mkdir(answer_path)
+    check_folder(os.path.abspath(NULL_PATH), answer_path)
+    print("Time: {}".format(time.time() - start_time))
